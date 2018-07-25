8beb7055c6c01ca8f2cf0065607b22d159e30586
azzkikr45 <38996788+azzkikr45@users.noreply.github.com>
2018-07-26 00:57:31 +0900

    Create kor.php
    Korean translation (Jul 26, 2018)

---
+++ kor.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined("IN_JCM")) die();
+
+$lang = [
+    'html_lang' => 'kor',
+    'title' => '자바스크립트 기반, 웹 채굴 브라우저 (베타)',
+    'desc' => '자바스크립트를 기반한 BBSCoin 웹 채굴 브라우저',
+    'keywords' => 'mining,miner,monero,bbscoin,cryptocurrency,coin',
+    'page_title' => '자바스크립트 기반, 웹 채굴 브라우저',
+    'page_intro' => 'PC/맥/모바일 브라우저로 <a href="https://bbscoin.xyz" target="_blank">BBSCoin</a>을 채굴해 보세요',
+    'status' => '상태',
+    'hashes' => '총 해쉬 전송량',
+    'hashrate' => '해쉬값',
+    'pending' => '대기중',
+    'pending_desc' => '1만개 BBS 채굴 후 전송 가능',
+    'paid' => '전송 완료',
+    'settings' => '설정',
+    'walletaddress' => '지갑 주소',
+    'registerwallet' => '(지갑이 없으세요? <a href="https://bbs.money" target="_blank">등록</a>)',
+    'threads' => '스레드',
+    'autodetect' => '자동 설정',
+    'cpu' => 'CPU 사용량 비율',
+    'start' => '채굴 및 스크립트 실행',
+    'stop' => '채굴 중지',
+    'embedded_script' => '사이트 탑재 스크립트',
+    'copy_code' => '여러분의 사이트에 아래 코드를 복사해 BBS를 채굴해 보세요. 다만, 법적 문제가 되지 않기 위해서 사이트 가입자에게 미리 공지할 것을 권고합니다.'
+    'misc' => '기타',
+    'invite' => '초대 링크',
+    'inviteinfo' => '링크를 복사해서 주변분들을 초대하세요! BBS를 채굴해 보세요!',
+];
