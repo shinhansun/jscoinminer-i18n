c8761a7b0e3970c3e137e9fc4b7904b99a685de3
こはね <hatoyakohane@gmail.com>
2018-07-26 10:58:39 +0800

    Merge pt_BR from #c7257de

---
+++ pt_BR.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined("IN_JCM")) die();
+
+$lang = [
+    'html_lang' => 'pt_BR',
+    'title' => 'Minerador WEB, Baseado em Javascript. Versão: (Beta)',
+    'desc' => 'Minere Online BBSCoin através do seu navegador WEB',
+    'keywords' => 'mineracao,minerar,monero,bbscoin,cryptocurrency,coin',
+    'page_title' => 'Minerador Web Baseado em Javascript para Mineração no Navegador',
+    'page_intro' => 'Você pode Minerar<a href="https://bbscoin.xyz" target="_blank">BBSCoin</a> com seu PC ou Celular Usando o Navegador WEB',
+    'status' => 'Status',
+    'hashes' => 'Total Hashes Enviados',
+    'hashrate' => 'Taxa de Hash',
+    'pending' => 'Pendente',
+    'pending_desc' => 'Pagamento Após 10,000 BBS',
+    'paid' => 'Pago',
+    'settings' => 'Configurações',
+    'walletaddress' => 'Endereço da Carteira',
+    'registerwallet' => '(Ainda não tem uma carteira? <a href="https://bbs.money" target="_blank">Crie sua Carteira Aqui.</a>)',
+    'threads' => 'Processadores',
+    'autodetect' => 'Auto detectar',
+    'cpu' => 'Porcentagem de Uso da CPU',
+    'start' => 'Iniciar Mineração e Gerar script',
+    'stop' => 'Parar Mineração',
+    'embedded_script' => 'Incorporação ao Site',
+    'copy_code' => 'Copie o código abaixo para o seu site, é importante você alertar seus usuários em um lugar de destaque sobre mineração da web, evite a violar a lei.',
+    'misc' => 'Misc',
+    'invite' => 'Convidar',
+    'inviteinfo' => 'Copie o Link e Convide Outras Pessoas! Ganhe BBS!',
+];
