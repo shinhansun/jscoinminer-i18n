b54133016be9a6b6ae61a71533b7f631438ebdc2
shinhansun <43377663+shinhansun@users.noreply.github.com>
2019-02-10 03:14:00 +0900

    Russia JSCoinminer
    Russia JSCoinminer

---
+++ ru.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined("IN_JCM")) die();
+
+$lang = array_merge($lang, [
+    'html_lang' => 'ru',
+    'html_dir' => 'ltr', //left to right
+    'title' => 'Основанный на Javascript, веб-браузер Mining (бета)',
+    'desc' => 'Веб-браузер майнинг на основе JavaScript для BBSCoin',
+    'keywords' => 'добыча полезных ископаемых, шахтер, monero, bbscoin, криптовалюта, монета',
+    'page_title' => 'Основанный на Javascript, веб-браузер майнинг',
+    'page_intro' => 'Ты можешь мой <a href="https://bbscoin.xyz" target="_blank">BBSCoin</a> с вашим ПК / Mac / Мобильным браузером',
+    'status' => 'Status',
+    'hashes' => 'Всего отправлено хэшей',
+    'hashrate' => 'Скорость хэширования',
+    'pending' => 'Pending',
+    'pending_desc' => 'Оплата после 5000 BBS',
+    'paid' => 'оплаченный',
+    'settings' => 'настройки',
+    'walletaddress' => 'Адрес кошелька',
+    'registerwallet' => '(Нет кошелька? <a href="https://bbs.money" target="_blank">Зарегистрируйтесь здесь</a>)',
+    'threads' => 'Threads',
+    'autodetect' => 'Автоматическое распознавание',
+    'cpu' => 'Процент использования процессора',
+    'start' => 'Начать майнинг и создать скрипт',
+    'stop' => 'Stop mining',
+    'embedded_script' => 'Сайт встроенный скрипт',
+    'copy_code' => 'Скопируйте приведенный ниже код на свой сайт, также вы должны предупредить своих пользователей на видном месте о веб-майнинге, предотвращении нарушений закона.',
+    'misc' => 'Misc',
+    'invite' => 'приглашение',
+    'inviteinfo' => 'Скопируйте Пригласить ссылку, чтобы пригласить других! Заработай BBS!',
+]);
